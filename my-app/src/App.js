437f19c98507a01cd6a123f13c83ef67365cabbc
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2024-10-30 14:07:53 -0500

    react routes open

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id='root' className="App">
+      <div className="side"></div>
+      <div className='mainContent'>
+        <p className="p">I think i will start with this</p>
+        <p className="p">This tests the extending thing</p>
+        <p className="p">Hi sean</p>
+      </div>
+      <div className="side"></div>
+    </div>
+  );
+}
+
+export default App;
+
+

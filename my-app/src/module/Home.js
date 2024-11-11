0106e1d0e33b716afa2b00b294430b56d4007c6c
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2024-11-11 12:39:44 -0600

    alt text needs updating

--- my-app/src/module/Home.js
+++ my-app/src/module/Home.js
@@ -14,7 +14,7 @@ function Home() {
       <h1>Let's Grow Together</h1>
       <p>White River Valley Farms is a humble Community Supported Agriculture (CSA) project led by Ozarks resident Michael Maniatis. After growing up in the northwestern United States and moving around the country as a young adult, Michael fell in love with the Ozarks and found himself wanting to give back to his new home. Inspired by the likes of Millsap Farms and fueled by a newfound appreciation for local agriculture, Michael founded White River Valley Farms to grow and share local, fresh produce.</p>
       <p>The mission of White River Valley Farms is simple: helping the Ozarks grow together. Nothing brings a community together quite like local agriculture. Our CSA wants to contribute to the Ozarks with quality produce and neighborly love.</p>
-      <img src={HomeScreen} style={{width:'81px', height: '61px', margin: '10px'}}/>
+      <img src={HomeScreen} style={{width:'81px', height: '61px', margin: '10px'}} alt="Farm image."/>
     </div>
   );
 }

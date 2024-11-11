9330fb5243ca96137da85e054a2353d0d32dacaf
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2024-11-11 14:07:17 -0600

    Our_product page stuff

--- my-app/src/module/Our_product.js
+++ my-app/src/module/Our_product.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import { Link } from 'react-router-dom';
-
+import Corn from '../Images/corn.png';
+import Garlic from '../Images/garlic.png'
 
 function OurProduct() {
   return (
@@ -12,7 +13,12 @@ function OurProduct() {
         <Link to='/join'>Join Us</Link>
         <Link to='/CSA'>What is a CSA</Link>
       </nav>
-      <p>hello we are on the product page</p>
+      <p>Corn</p>
+      <p>Product Harvest Period</p>
+      <img scr={Corn} style={{width:'69px', height: '91px', margin: '10px'}} alt="Fresh Corn in Wheel Barral."/>
+      <p>Garlic</p>
+      <p>Product Harvest Period</p>
+      <img scr={Garlic} style={{width:'73px', height: '98px', margin: '10px'}} alt="Many Galic bulbs on table."/>
     </div>
   );
 }

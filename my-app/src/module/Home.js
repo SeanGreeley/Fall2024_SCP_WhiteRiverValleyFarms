0564866f0011165ebffbb93df1dbb47a451e7625
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2024-11-11 12:37:12 -0600

    image work

--- my-app/src/module/Home.js
+++ my-app/src/module/Home.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-let HomeScreen = require('../Images/home_Screen.png')
-//my Image does not work idk why, i tried the import version too
+import HomeScreen from '../Images/home_Screen.png';
+
 function Home() {
   return (
     <div id='root' className="App">
@@ -14,7 +14,7 @@ function Home() {
       <h1>Let's Grow Together</h1>
       <p>White River Valley Farms is a humble Community Supported Agriculture (CSA) project led by Ozarks resident Michael Maniatis. After growing up in the northwestern United States and moving around the country as a young adult, Michael fell in love with the Ozarks and found himself wanting to give back to his new home. Inspired by the likes of Millsap Farms and fueled by a newfound appreciation for local agriculture, Michael founded White River Valley Farms to grow and share local, fresh produce.</p>
       <p>The mission of White River Valley Farms is simple: helping the Ozarks grow together. Nothing brings a community together quite like local agriculture. Our CSA wants to contribute to the Ozarks with quality produce and neighborly love.</p>
-      <image src={HomeScreen} style={{width:'81px', height: '61px', margin: '10px'}}/>
+      <img src={HomeScreen} style={{width:'81px', height: '61px', margin: '10px'}}/>
     </div>
   );
 }

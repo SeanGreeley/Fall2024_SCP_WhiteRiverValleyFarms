71c5b12fb8a119f55127a880a92f4941d1930737
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2024-11-13 12:29:15 -0600

    Styling Changes

--- my-app/src/module/What_is_CSA.js
+++ my-app/src/module/What_is_CSA.js
@@ -1,17 +1,40 @@
 import allText from '../textElements.json';
 import Header from './header';
 import Footer from './footer';
+import colors from '../colors.json';
 
 function WhatIsACSA() {
+  const paragraphStyles = {
+    color: colors.charcoal,
+    textAlign: 'left',
+    fontSize: '24px',
+    backgroundColor: colors.ivory,
+    padding: '1em',
+    width: '50%',
+  };
+  const headerStyles = {
+    color: colors.orange,
+    backgroundColor: colors.ivory,
+    padding: '1em'
+  };
+  const textboxStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.ivory,
+  };
     return (
-        <div id='root' className="App">
+        <div id='root' className="App" styles={{backgroundColor: colors.ivory}}>
         <Header/>
-        <h1>{allText['csa-info'].header}</h1>
-          <p>{allText['csa-info']['body-1']}</p>
-          <p>{allText['csa-info']['body-2']}</p>
-          <p>{allText['csa-info']['body-3']}</p>
-          <p>{allText['csa-info']['body-4']}</p>
-          <p>{allText['csa-info']['body-5']}</p>
+        <h1 style={headerStyles}>{allText['csa-info'].header}</h1>
+          <div style={textboxStyles}>
+          <p style={paragraphStyles}>{allText['csa-info']['body-1']}</p>
+          <p style={paragraphStyles}>{allText['csa-info']['body-2']}</p>
+          <p style={paragraphStyles}>{allText['csa-info']['body-3']}</p>
+          <p style={paragraphStyles}>{allText['csa-info']['body-4']}</p>
+          <p style={paragraphStyles}>{allText['csa-info']['body-5']}</p>
+          </div>
           <Footer/>
         </div>
     );

6661de3a305b0f187c55cddab0d3ca043e27ee2d
TaylorBD-314 <taylor.boese@gmail.com>
2024-11-18 17:41:11 -0600

    Adding Logo to footer

--- my-app/src/module/footer.js
+++ my-app/src/module/footer.js
@@ -1,5 +1,6 @@
 import allText from '../textElements.json';
 import colors from '../colors.json'
+import Logo from '../Images/Logo_Edit_2.png';
 
 function Footer() {
     const styles = {
@@ -10,9 +11,17 @@ function Footer() {
         fontSize: '3vh',
         marginTop: '5vh'
     };
+    const imgStyles ={
+      width: '200px', 
+      height: '200px', 
+      objectFit: 'cover', 
+      padding: '10px',
+      backgroundColor: colors.ivory
+    };
     return (
         <div>
             <p style={styles}>{allText.footer}</p>
+            <img src={Logo} style={imgStyles} alt={p.alt} title={p.alt}/>
         </div>
     );
 }
